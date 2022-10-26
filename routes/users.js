6b1c015cedd06c658949eb83384d270a2a287af3
vamsikolusu <vkolusu27@gmail.com>
2022-10-26 18:28:37 -0500

    this is updata file

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var time=0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  time=time+1;
+  res.send('User accesses are:'+time);
+
 });
 
 module.exports = router;
